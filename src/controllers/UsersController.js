b91a8481191a4d1928f4bf97d1caaf2274ec8555
Matheus <matheus-rodrigues37@live.com>
2022-10-17 16:10:28 -0300

    user controller

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,5 +1,6 @@
 import knex from "../knex";
-import { hash } from "bcryptjs";
+import { hash, compare } from "bcryptjs";
+import AppError from "../utils/AppError";
 
 class UsersController {
   async create(req, res) {
@@ -24,6 +25,59 @@ class UsersController {
     } catch (error) {
       console.error(error.message);
     }
+  }
+  async update(req, res) {
+    const { name, email, password, old_password } = req.body;
+    const user_id = req.user.id;
+
+    const user = await knex("users").where({ id: user_id });
+
+    if (!user) {
+      throw new AppError("Usuário não encontrado");
+    }
+
+    const userWithUpdatedEmail = await knex("users").where({ email });
+
+    if (userWithUpdatedEmail && userWithUpdatedEmail.id !== user.id) {
+      throw new AppError("Este e-mail já está em uso.");
+    }
+
+    await knex("users")
+      .where({ id: user_id })
+      .update({
+        name: user.name ?? name,
+        email: user.email ?? email,
+      });
+
+    if (password && !old_password) {
+      throw new AppError(
+        "Você precisa informar a senha antiga para definir a nova senha."
+      );
+    }
+
+    if (password && old_password) {
+      const checkOldPassword = await compare(old_password, user.password);
+
+      if (!checkOldPassword) {
+        throw new AppError("A senha antiga não confere.");
+      }
+
+      const hashedNewPassword = await hash(password, 8);
+
+      await knex("users").where({ id: user_id }).update({
+        password: hashedNewPassword,
+      });
+    }
+    await knex("users")
+      .where({ id: user_id })
+      .update({
+        name: user.name ?? name,
+        email: user.email ?? email,
+        password: user.password ?? password,
+        updated_at: new Date(),
+      });
+
+    return res.json({ user });
   }
 }
 
