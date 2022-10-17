3fe0c7d38b5921354c8ce1628b9252527d2e26f9
Matheus <matheus-rodrigues37@live.com>
2022-10-17 17:34:59 -0300

    finish

--- src/controllers/SessionsController.js
+++ src/controllers/SessionsController.js
@@ -6,21 +6,25 @@ import { jwt } from "../configs/auth";
 
 class SessionsController {
   async create(req, res) {
-    const { email, password } = req.body;
-    const user = await knex("users").where({ email }).first();
-    if (!user) {
-      throw new AppError("E-mail e/ou senha incorreta.", 401);
+    try {
+      const { email, password } = req.body;
+      const user = await knex("users").where({ email }).first();
+      if (!user) {
+        throw new AppError("E-mail e/ou senha incorreta.", 401);
+      }
+      const passwordMatched = await compare(password, user.password);
+      if (!passwordMatched) {
+        throw new AppError("E-mail e/ou senha incorreta.", 401);
+      }
+      const { secret, expiresIn } = jwt;
+      const token = sign({}, secret, {
+        subject: String(user.id),
+        expiresIn,
+      });
+      res.status(201).json({ token, user });
+    } catch (e) {
+      console.log(e.message);
     }
-    const passwordMatched = await compare(password, user.password);
-    if (!passwordMatched) {
-      throw new AppError("E-mail e/ou senha incorreta.", 401);
-    }
-    const { secret, expiresIn } = jwt;
-    const token = sign({}, secret, {
-      subject: String(user.id),
-      expiresIn,
-    });
-    res.status(201).json({ token, user });
   }
 }
 
--- src/knex/migrations/20221015235502_createUsers.js
+++ src/knex/migrations/20221015235502_createUsers.js
@@ -6,7 +6,7 @@ exports.up = function (knex) {
   return knex.schema.createTable("users", (table) => {
     table.increments("id").primary();
     table.string("name").notNullable();
-    table.string("email").notNullable().unique();
+    table.string("email").notNullable();
     table.string("password").notNullable();
     table.string("avatar").nullable();
     table.timestamps(true, true);
