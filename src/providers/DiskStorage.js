aebbbe27d88d26c7f86b3177e836bdaa53a48c47
Matheus <matheus-rodrigues37@live.com>
2022-10-17 15:18:10 -0300

    providers

---
+++ src/providers/DiskStorage.js
@@ -0,0 +1,25 @@
+import fs from "fs";
+import path from "path";
+import uploadConfig from "../configs/upload";
+
+class DiskStorage {
+  async saveFile(file) {
+    await fs.promises.rename(
+      path.resolve(uploadConfig.TMP_FOLDER, file),
+      path.resolve(uploadConfig.UPLOADS_FOLDER, file)
+    );
+    return file;
+  }
+
+  async deleteFile(file) {
+    const filePath = path.resolve(uploadConfig.UPLOADS_FOLDER, file);
+    try {
+      await fs.promises.stat(filePath);
+    } catch {
+      return;
+    }
+    await fs.promises.unlink(filePath);
+  }
+}
+
+export default DiskStorage;
