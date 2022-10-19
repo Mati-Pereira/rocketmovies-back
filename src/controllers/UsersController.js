1f74b6996d46a0d092946c6ad40a2e6250e689af
Matheus <matheus-rodrigues37@live.com>
2022-10-19 16:32:20 -0300

    up

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -7,6 +7,11 @@ class UsersController {
     try {
       const { name, email, password } = req.body;
       const hashedPassword = await hash(password, 8);
+      const user = await knex("users");
+      const userWithUpdatedEmail = await knex("users").where({ email });
+      if (userWithUpdatedEmail && userWithUpdatedEmail.id !== user.id) {
+        throw new AppError("Este e-mail já está em uso.");
+      }
       await knex("users").insert({
         name,
         email,
