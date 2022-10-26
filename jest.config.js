35bced55fd219e6b5eb29e476a8635e4232c09e9
Matheus <matheus-rodrigues37@live.com>
2022-10-26 14:47:44 -0300

    small confiig on jest

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   bail: true,
+  modulePathIgnorePatterns: ["<rootDir>/dist/", "<rootDir>/node_modules/"],
   coverageProvider: "v8",
   testMatch: ["<rootDir>/**/*.spec.js"],
   transform: { "\\.(js)$": "@sucrase/jest-plugin" },
