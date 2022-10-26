512031e864dde383d6a27920b2419159ed909314
Matheus <matheus-rodrigues37@live.com>
2022-10-26 16:28:13 -0300

    toFix: segundo teste

--- src/services/UserCreateService.spec.js
+++ src/services/UserCreateService.spec.js
@@ -1,15 +1,35 @@
 import UserCreateService from "./UserCreateService";
 import userRepositoryInMemory from "../repositories/UserRepositoryInMemory";
+import AppError from "../utils/AppError";
 
-it("user should be create", async () => {
-  const user = {
-    name: "User test",
-    email: "user@test.com",
-    password: "2645789",
-  };
+describe("UserCreateService", () => {
+  it("user should be create", async () => {
+    const user = {
+      name: "User test",
+      email: "user@test.com",
+      password: "2645789",
+    };
 
-  const userCreateService = new UserCreateService(userRepositoryInMemory);
-  const userCreated = await userCreateService.execute(user);
+    const userCreateService = new UserCreateService(userRepositoryInMemory);
+    const userCreated = await userCreateService.execute(user);
 
-  expect(userCreated).toHaveProperty("id");
+    expect(userCreated).toHaveProperty("id");
+  });
+  it("user should not be able to create a email while there is a email in database", async () => {
+    const user1 = {
+      name: "User1",
+      email: "user@test.com",
+      password: "4564561",
+    };
+    const user2 = {
+      name: "User2",
+      email: "user@test.com",
+      password: "4554456",
+    };
+    const userCreateService = new UserCreateService(userRepositoryInMemory);
+    await userCreateService.execute(user1);
+    expect(async () => {
+      await userCreateService.execute(user2);
+    }).rejects.toEqual(new AppError("Este e-mail já está em uso."));
+  });
 });
