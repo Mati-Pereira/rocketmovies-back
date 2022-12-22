a401d62b3d655192ea819ac09f4a520a1c0d19b0
Matheus <matheus-rodrigues37@live.com>
2022-12-22 12:35:37 -0300

    up

--- knexfile.js
+++ knexfile.js
@@ -1,10 +1,9 @@
 const path = require("path");
-
-require("dotenv").config({ path: path.resolve(".env.local") });
+require("dotenv").config({ path: path.resolve(".env") });
 
 module.exports = {
   client: "pg",
-  connection: process.env.DATABASE_URL || "error",
+  connection: process.env.DATABASE_URL,
   pool: {
     min: 2,
     max: 10,
