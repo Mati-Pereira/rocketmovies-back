68fe01f31ea1031286f9a75a7ff4bcb210cb3395
Matheus <matheus-rodrigues37@live.com>
2022-12-22 13:21:37 -0300

    fix

--- src/routes/notes.routes.js
+++ src/routes/notes.routes.js
@@ -1,14 +1,13 @@
 const { Router } = require("express");
 
 const notesController = require("../controllers/NotesController");
-const tagsController = require("../controllers/TagsController");
 const ensureAuthenticated = require("../middlewares/ensureAuthenticated");
 
 const notesRoutes = Router();
 
 notesRoutes.use(ensureAuthenticated);
 
-notesRoutes.post("/", notesController.create, tagsController.create);
+notesRoutes.post("/", notesController.create);
 notesRoutes.get("/:id", notesController.show);
 notesRoutes.delete("/:id", notesController.delete);
 notesRoutes.get("/", notesController.index);
